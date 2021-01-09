4d3b7ddd656ae8e3e57778377436bc432c46a564
Juho Kim <juho-kim@outlook.com>
2021-01-09 10:54:20 -0500

    added requests to requirements.txt

--- example_players.py
+++ example_players.py
@@ -1,16 +1,19 @@
-import random
-import numpy as np
-from typing import List, Dict, Optional, Tuple
 import copy
+import random
+from typing import Dict, List, Optional, Tuple
 
-from site_location import SiteLocationPlayer, Store, SiteLocationMap, euclidian_distances, attractiveness_allocation
+import numpy as np
+
+from site_location import SiteLocationMap, SiteLocationPlayer, Store, attractiveness_allocation, euclidian_distances
+
 
 class RandomPlayer(SiteLocationPlayer):
     """
     Player attempts to place the maximum stores, with each store type and
     position chosen randomly.
     """
-    def place_stores(self, slmap: SiteLocationMap, 
+
+    def place_stores(self, slmap: SiteLocationMap,
                      store_locations: Dict[int, List[Store]],
                      current_funds: float):
         stores = []
@@ -30,7 +33,8 @@ class MaxDensityPlayer(SiteLocationPlayer):
 
     Store type will always be the largest one it can afford.
     """
-    def place_stores(self, slmap: SiteLocationMap, 
+
+    def place_stores(self, slmap: SiteLocationMap,
                      store_locations: Dict[int, List[Store]],
                      current_funds: float):
         store_conf = self.config['store_config']
@@ -52,12 +56,13 @@ class MaxDensityPlayer(SiteLocationPlayer):
         for player, player_stores in store_locations.items():
             for player_store in player_stores:
                 all_stores_pos.append(player_store.pos)
-        
-        sorted_indices = tuple(map(tuple, np.dstack(np.unravel_index(np.argsort(slmap.population_distribution.ravel()), slmap.size))[0][::-1]))
+
+        sorted_indices = tuple(map(tuple, np.dstack(
+            np.unravel_index(np.argsort(slmap.population_distribution.ravel()), slmap.size))[0][::-1]))
         for max_pos in sorted_indices:
             too_close = False
             for pos in all_stores_pos:
-                dist = np.sqrt(np.square(max_pos[0]-pos[0]) + np.square(max_pos[1]-pos[1]))
+                dist = np.sqrt(np.square(max_pos[0] - pos[0]) + np.square(max_pos[1] - pos[1]))
                 if dist < min_dist:
                     too_close = True
             if not too_close:
@@ -68,7 +73,8 @@ class CopycatPlayer(SiteLocationPlayer):
     """ 
     Player places an identical store at the location of a random opponent's store.
     """
-    def place_stores(self, slmap: SiteLocationMap, 
+
+    def place_stores(self, slmap: SiteLocationMap,
                      store_locations: Dict[int, List[Store]],
                      current_funds: float):
 
@@ -76,25 +82,27 @@ class CopycatPlayer(SiteLocationPlayer):
         for store in store_locations[self.player_id]:
             self_stores_pos.append(store.pos)
 
-        opp_store_locations = {k:v for (k,v) in store_locations.items() if k != self.player_id}
+        opp_store_locations = {k: v for (k, v) in store_locations.items() if k != self.player_id}
         opp_all_stores = []
         for player, player_stores in opp_store_locations.items():
             for player_store in player_stores:
                 if player_store.pos not in self_stores_pos:
                     opp_all_stores.append(player_store)
         if not opp_all_stores:
-            self.stores_to_place =  []
+            self.stores_to_place = []
             return
         else:
             self.stores_to_place = [random.choice(opp_all_stores)]
             return
-        
+
+
 class AllocSamplePlayer(SiteLocationPlayer):
     """
     Agent samples locations and selects the highest allocating one using
     the allocation function. 
     """
-    def place_stores(self, slmap: SiteLocationMap, 
+
+    def place_stores(self, slmap: SiteLocationMap,
                      store_locations: Dict[int, List[Store]],
                      current_funds: float):
         store_conf = self.config['store_config']
@@ -104,7 +112,7 @@ class AllocSamplePlayer(SiteLocationPlayer):
         for i in range(num_rand):
             x = random.randint(0, slmap.size[0])
             y = random.randint(0, slmap.size[1])
-            sample_pos.append((x,y))
+            sample_pos.append((x, y))
         # Choose largest store type possible:
         if current_funds >= store_conf['large']['capital_cost']:
             store_type = 'large'
